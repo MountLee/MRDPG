672b8bba0a82334ff4b5e03d46b49775e134c917
MountLee <wanshanl@andrew.cmu.edu>
2024-06-10 18:49:21 +0800

    Delete cpd_dirichlet.sh

--- cpd_dirichlet.sh
+++
@@ -1,7 +0,0 @@
-R CMD BATCH cpd_dirichlet_d_2.R &
-R CMD BATCH cpd_dirichlet_d_6.R &
-R CMD BATCH cpd_dirichlet_L_2.R &
-R CMD BATCH cpd_dirichlet_L_3.R &
-R CMD BATCH cpd_dirichlet_L_4.R &
-R CMD BATCH cpd_dirichlet_n_25.R &
-R CMD BATCH cpd_dirichlet_n_75.R &
